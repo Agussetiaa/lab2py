a579021f363263ee0a2b1687ad6e7c64da197853
AGUS SETIAWAN <115542822+Agussetiaa@users.noreply.github.com>
2022-10-19 19:14:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ input = ("masukan jari-jari lingkaran")
 pi = 3.14
 l = pi * int(r) * int (r)
 k = 2 * pi * int(r)
-print("luas lingkaran :",l)
-print("keliling lingkarang",k) 
+print("luas lingkaran:",l)
+print("keliling lingkarang:",k) 
